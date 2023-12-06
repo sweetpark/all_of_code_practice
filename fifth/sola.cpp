c655dc94003b7f9656a75e6e033513adf33dad0e
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2023-12-06 23:05:06 +0900

    marine create pracitce

--- fifth/sola.cpp
+++ fifth/sola.cpp
@@ -1,7 +1,71 @@
 #include <iostream>
 
-class Marine {
+class Marine{
+
     int hp;
     int coord_x, coord_y;
-    
+    int damage;
+    bool is_dead;
+
+
+    public:
+        Marine();
+        Marine(int x, int y);
+
+        int attack();
+        void be_attacked(int damage_earn);
+        void move(int x, int y);
+
+
+
+        void show_status();
+};
+
+//생성자 (인자 x)
+Marine::Marine(){
+    hp = 50;
+    coord_x = coord_y = 0;
+    damage =5;
+    is_dead = false;
+}
+
+//생성자 (인자 o) 
+Marine::Marine(int x, int y){
+    coord_x = x;
+    coord_y = y;
+    hp = 50;
+    damage = 5;
+    is_dead = false;
+}
+
+void Marine::move(int x, int y){
+    coord_x = x;
+    coord_y = y;
+}
+
+int Marine::attack() { return damage; }
+void Marine::be_attacked(int damage_earn) {
+    hp -= damage_earn;
+    if (hp <= 0) is_dead = true;
+}
+
+void Marine::show_status(){
+    std::cout << " *** Marine *** " << std::endl;
+    std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) " << std::endl;
+    std::cout << " HP : " << hp << std::endl;
+}
+
+
+int main() {
+    Marine marine1(2,3);
+    Marine marine2(3,5);
+
+    marine1.show_status();
+    marine2.show_status();
+
+    std::cout << std::endl << "마린 1 이 마린 2 를 공격! " << std::endl;
+    marine2.be_attacked(marine1.attack());
+
+    marine1.show_status();
+    marine2.show_status();
 }
